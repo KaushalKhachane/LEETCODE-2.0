f69d1cf4cf222eb03b846e709b134500373d9d14
Kaushal Khachane <khachaneks22@gmail.com>
2023-12-25 20:20:33 +0530

    Combinational Sum II

--- Leetcode40.java
+++ Leetcode40.java
@@ -1,31 +1,26 @@
 class Solution {
-    void solve(int[] can, int k, List<List<Integer>> ans, int s, int sum, List<Integer> list, Set<List<Integer>> uq) {
-        if (sum > k)
-            return;
-        if (sum == k) {
-            if (uq.add(new ArrayList<>(list))) {
-
-                ans.add(new ArrayList<>(list));
-            }
-            return;
-        }
-        for (int i = s; i < can.length; i++) {
-            if (i > s && can[i] == can[i - 1]) {
-                continue; // Skip duplicates to avoid duplicate combinations
-            }
-
-            list.add(can[i]);
-            solve(can, k, ans, i + 1, sum + can[i], list, uq);
-            list.remove(list.size() - 1);
-        }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinations(0, candidates, target, ans, new ArrayList<>());
+        return ans;
     }
 
-    public List<List<Integer>> combinationSum2(int[] can, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(can);
-        Set<List<Integer>> uq = new HashSet<>();
-        solve(can, target, ans, 0, 0, list, uq);
-        return ans;
+    void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds ){
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i = ind; i < arr.length; i++){
+            if(i > ind && arr[i] == arr[i-1]){
+                continue;
+            }
+            if(arr[i] > target){
+                break;
+            }
+            ds.add(arr[i]);
+            findCombinations(i+1,arr,target-arr[i], ans,ds);
+            ds.remove(ds.size()-1);
+        }
     }
 }
