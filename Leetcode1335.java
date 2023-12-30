3354af04fc7b7bbced7a83ba5824c9353f2b4a5b
Kaushal Khachane <khachaneks22@gmail.com>
2023-12-30 17:05:18 +0530

    1335 modify

--- Leetcode1335.java
+++ Leetcode1335.java
@@ -1,39 +1,32 @@
 class Solution {
     public int minDifficulty(int[] jobDifficulty, int d) {
-        if (jobDifficulty.length < d) {
+        
+        int jobs = jobDifficulty.length;
+
+        if (jobs < d) {
             return -1;
         }
-        int len = jobDifficulty.length;
-        int sum = 0;
-        for (int i = 0; i < jobDifficulty.length; i++) {
-            sum += jobDifficulty[i];
-        }
-        if (sum == 0) {
-            return 0;
-        }
-        return helper(jobDifficulty, d, 0);
-    }
 
-    private int helper(int[] jd, int daysLeft, int idx) {
-        int len = jd.length;
-        if (daysLeft == 1) {
-            int num = 0;
-            for (int i = idx; i < len; i++) {
-                num = Math.max(num, jd[i]);
+        int dp[][] = new int[d][jobs];
+
+        dp[0][0] = jobDifficulty[0];
+        for(int i = 1; i < jobs; i++) {
+            dp[0][i] = Math.max(dp[0][i-1], jobDifficulty[i]);
+        }
+
+        for(int days = 1; days < d; days++) {
+
+            for(int i = days; i < jobs; i++) {
+                int Max = jobDifficulty[i];
+                dp[days][i] = Integer.MAX_VALUE;
+
+                for(int j = i; j >= days; j--) {
+                    Max = Math.max(Max, jobDifficulty[j]);
+                    dp[days][i] = Math.min(dp[days][i], dp[days - 1][j - 1] + Max);
+                }
             }
-            return num;
         }
 
-        int max = jd[idx];
-        daysLeft--;
-        int stop = len - idx - daysLeft + 1;
-
-        int res = Integer.MAX_VALUE;
-        for (int i = 1; i < stop; i++) {
-            max = Math.max(max, jd[idx + i - 1]);
-            int other = helper(jd, daysLeft, idx + i);
-            res = Math.min(res, other + max);
-        }
-        return res;
+        return dp[d-1][jobs-1];
     }
 }
