5ad3830e22bd34a5fb4af6a042f00cb688ef7e6f
Kaushal Khachane <khachaneks22@gmail.com>
2024-02-27 11:21:13 +0530

    insert pos

--- Leetcode35.java
+++ Leetcode35.java
@@ -1,18 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
+        int n = nums.length;
+        int low = 0;
+        int end = n - 1;
+        int ans = n;
+        while(low <= end){
+            int mid = low + (end - low) / 2;
+            if(nums[mid] >= target){
+                ans = mid;
+                end = mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
-        int j;
-        for(j=0;j<nums.length;j++){
-            if(nums[j]>target){
-                return j;
-            }
-        }
 
-        return j;
+        return ans;
     }
 }
