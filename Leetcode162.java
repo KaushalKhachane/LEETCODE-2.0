f487aa0c031bde4ec1ea83fe16a5b00464246d80
Kaushal Khachane <khachaneks22@gmail.com>
2024-02-29 00:19:19 +0530

    162

---
+++ Leetcode162.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        // edge cases
+        if(n==1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-2] < nums[n-1]) return n-1;
+
+
+        int low = 1;
+        int end = n - 2;
+        
+        while(low <= end){
+            int mid =  low + (end - low) / 2;
+
+            if((nums[mid-1] < nums[mid]) && (nums[mid] > nums[mid+1])){
+                return mid;
+            }
+
+            if(nums[mid] > nums[mid-1]){
+                low = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
