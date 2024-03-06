a6bc046a5a6a577a13c1652152a13d4dbfca9446
Kaushal Khachane <khachaneks22@gmail.com>
2024-03-06 18:01:36 +0530

    detect cycle

--- Leetcode141.java
+++ Leetcode141.java
@@ -11,23 +11,23 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-
-        // if ll is empty or single node ll then no cycle exists then return false
         if(head == null || head.next == null){
             return false;
-        }        
-        ListNode slow = head;
-        ListNode fast = head;
-
-        // if cycle then for sure slow and fast meet at some node
-        while(fast.next != null && fast.next.next != null){
-            fast =  fast.next.next;
-            slow = slow.next;
-            if(fast == slow){
-                return true;
-            }
         }
 
-        return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(slow != fast){
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+        
     }
 }
