8eb4cfed1579f6e1bcfcb31f0f0c954c4cb95a11
Kaushal Khachane <khachaneks22@gmail.com>
2024-04-10 10:42:40 +0530

    ticket

--- Leetcode2073.java
+++ Leetcode2073.java
@@ -1,5 +1,27 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
+         int time = 0;
+         while(true){
+             for(int i = 0; i < tickets.length; i++){
+                 if(tickets[i] > 0){
+                     tickets[i]--;
+                     time++;
+                 }
+                 if(tickets[k] == 0){
+                     return time;
+                 }
+             }
+         }
+         
+         // return time;  as while loop is running till true so this statement is unreachable so return true inside while loop only insted of break the loop and return time.
+     }
+ }
+ 
+ /* 
+         
+ 
+ 
+         ==
          int total=0;
          while (true){
              for(int i=0;i<tickets.length;i++){
@@ -13,5 +35,5 @@ class Solution {
  
              }
          }
-     }
- }
+ 
+ */
